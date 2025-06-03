8035f1c1d6d163f48fcac8ff72b7e49ad7838021
Denys Prudius <prdslife@gmail.com>
2025-06-03 23:11:56 +0300

    6 спринт

--- MovieQuiz/Models/QuizStepViewModel.swift
+++ MovieQuiz/Models/QuizStepViewModel.swift
@@ -2,7 +2,7 @@ import UIKit
 
 // для состояния "Вопрос показан"
 struct QuizStepViewModel {
-  let image: UIImage
-  let question: String
-  let questionNumber: String
+    let image: UIImage
+    let question: String
+    let questionNumber: String
 }
